ec217b7b68876ba0a1dd4c168d2f4b48936052d9
gurxt <jung_jungian@outlook.com>
2023-12-20 14:19:04 -0500

    set up server + db connection

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+import '#/db';
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.log('Server is listening on port ' + PORT);
+});
